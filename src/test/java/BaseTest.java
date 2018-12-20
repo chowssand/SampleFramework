cc4c39ad28bd0381cc83056febe4276f6ad08d31
Chowsan <chowsanjntu@gmail.com>
2018-12-20 15:43:19 +1100

    BaseTest is ready

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        System.setProperty("webdriver.chrome.driver","C:/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
+    }
+    @AfterClass
+    public static void tearDown(){
+        driver.close();
+    }
+
 }
