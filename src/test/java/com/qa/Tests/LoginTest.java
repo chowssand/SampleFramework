829558421efd5aaac86ea4caec209a7ff07c5b47
Chowsan <chowsanjntu@gmail.com>
2018-12-20 16:15:19 +1100

    PageObjects In-Progress

--- src/test/java/com/qa/Tests/LoginTest.java
+++ src/test/java/com/qa/Tests/LoginTest.java
@@ -1,10 +1,16 @@
 package com.qa.Tests;
 
+import com.qa.PageObjects.HomePage;
+import com.qa.PageObjects.LoginPage;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
     @Test
-    public void TestWithInvalidCredentials(){
+    public void TestWithValidCredentials(){
+        HomePage hp;
+        LoginPage lp = new LoginPage(driver);
+        hp = lp.LoginToSite("username","password");
+        hp.VerifyHomePage();
 
     }
 }
